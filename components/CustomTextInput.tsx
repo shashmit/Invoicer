d62ba65facc3b876678ccb4e5b082979b900e28b
Shashmit kumar <shaashmit@gmail.com>
2025-01-02 19:03:22 +0530

    re Tweak it

--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,28 +1,28 @@
-import { useController } from 'react-hook-form';
-import { View, Text, TextInput, TextInputProps } from 'react-native';
+// import { useController } from 'react-hook-form';
+// import { View, Text, TextInput, TextInputProps } from 'react-native';
 
-type CustomTextInputProps = {
-  label: string;
-  name: string;
-} & TextInputProps;
+// type CustomTextInputProps = {
+//   label: string;
+//   name: string;
+// } & TextInputProps;
 
-export default function CustomTextInput({ label, name, ...props }: CustomTextInputProps) {
-  const {
-    field: { onChange, onBlur, value },
-    fieldState: { error },
-  } = useController({ name });
+// export default function CustomTextInput({ label, name, ...props }: CustomTextInputProps) {
+//   const {
+//     field: { onChange, onBlur, value },
+//     fieldState: { error },
+//   } = useController({ name });
 
-  return (
-    <View className="gap-1">
-      <Text className="text-lg"> {label} </Text>
-      <TextInput
-        {...props}
-        className={`rounded border border-gray-300 p-4 ${props.className}`}
-        value={value}
-        onBlur={onBlur}
-        onChangeText={onChange}
-      />
-      <Text className="text-sm text-red-500"> {error?.message} </Text>
-    </View>
-  );
-}
+//   return (
+//     <View className="gap-1">
+//       <Text className="text-lg"> {label} </Text>
+//       <TextInput
+//         {...props}
+//         className={`rounded border border-gray-300 p-4 ${props.className}`}
+//         value={value}
+//         onBlur={onBlur}
+//         onChangeText={onChange}
+//       />
+//       <Text className="text-sm text-red-500"> {error?.message} </Text>
+//     </View>
+//   );
+// }
